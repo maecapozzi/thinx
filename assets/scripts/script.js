1d30548ded5ac1dd018f77c552cc7c48e9a45a47
maecapozzi <maecapozzi@gmail.com>
2016-07-18 12:48:15 -0400

    add changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,41 @@
 $( document ).ready(function() { 
-  $(".fa.fa-check.black").hide();
+  $(".fa.fa-check.black").hide(); 
   blackCheck();
   beigeCheck(); 
+
+
+
+
+
+var $animation_elements = $('.center-image');
+var $container = $(".image-container");
+
+function check_if_in_view() {
+  var container_height = $container.height();
+  var container_top_position = $container.scrollTop();
+  var container_bottom_position = (container_top_position + container_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= container_top_position) &&
+        (element_top_position <= container_bottom_position)) {
+      console.log($element);
+    } else {
+      console.log("hi");
+    }
+  });
+}
+
+$container.on('scroll resize', check_if_in_view);
+$container.trigger('scroll');
+
 });
+
 
 function blackCheck() { 
   $("#black-button").click(function(){  
@@ -17,3 +50,6 @@ function beigeCheck() {
     $(".fa.fa-check.black").hide();
   });
 }
+
+
+
